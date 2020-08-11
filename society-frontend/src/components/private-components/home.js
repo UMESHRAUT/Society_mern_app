946f49b7a267a7069c6810a6cdcd46adf0a36282
UMESHRAUT <rautumesh34@gmail.com>
2020-08-11 18:23:51 +0530

    style added

--- society-frontend/src/components/private-components/home.js
+++ society-frontend/src/components/private-components/home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom';
-import { json } from 'body-parser';
 import HomePage from './homePage/homePage';
 import AdminHome from './admin/components/AdminHome';
 
