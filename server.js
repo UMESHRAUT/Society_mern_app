a22542a490faffd9d87eeb78229ec32cd5819bde
UMESHRAUT <rautumesh34@gmail.com>
2020-08-12 21:03:12 +0530

    getting complaints

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV==="production"){
     app.use(express.static('society-frontend/build'));
 
     app.get('/',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'society-frontend','build','index.html'));
+        res.sendFile(path.resolve(__dirname+'/society-frontend/build/index.html'));
     });
 }
 
